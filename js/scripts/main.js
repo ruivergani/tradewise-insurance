4327f243390adf3338a25bdbf1f7f85f7a94e61d
ruivergani <ruiverganineto@gmail.com>
2023-10-14 10:40:11 +0100

    sub menu dropdown effect completed

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -13,6 +13,21 @@ const btnMobileMenu = document.getElementById("js-btn-menu");
 if(btnMobileMenu){
   btnMobileMenu.addEventListener("click", () => {
     btnMobileMenu.classList.toggle("is-active");
-    document.documentElement.classList.toggle("menuOpened");
+    document.documentElement.classList.toggle("opened-menu");
   });
+}
+const dropdownButtonMobileMenuItem = document.querySelectorAll(".js-mobile-menu-item");
+const dropdownAreaMobileMenu = document.querySelectorAll(".js-menu-dropdown-mobile");
+if(dropdownButtonMobileMenuItem && dropdownAreaMobileMenu){
+  dropdownButtonMobileMenuItem.forEach((button, index) => {
+    // Add click event to each button mobile menu dropdown
+    button.addEventListener("click", (event) => {
+      // Prevent default behavior tag a
+      event.preventDefault();
+      // add is-active class to the button
+      button.classList.toggle("is-active");
+      // open dropdown area to show submenu
+      dropdownAreaMobileMenu[index].classList.toggle("is-active");
+    })
+  })
 }
